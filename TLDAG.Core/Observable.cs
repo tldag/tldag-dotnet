8c3a58a853710ce4b8ac3a98bbc9d584549d42b9
rhjoerg <rhjoerg@gmail.com>
2021-10-08 09:28:31 +0200

    Added Observable

---
+++ TLDAG.Core/Observable.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TLDAG.Core
+{
+    public class Observable : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void RaisePropertyChanging([CallerMemberName] string? name = null)
+            { PropertyChanging?.Invoke(this, new(name)); }
+
+        protected void RaisePropertyChanged([CallerMemberName] string? name = null)
+            { PropertyChanged?.Invoke(this, new(name)); }
+    }
+}
