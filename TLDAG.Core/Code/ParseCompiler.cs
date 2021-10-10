c0f684a8a5ef065462ceb5bf2c973704655aa692
rhjoerg <rhjoerg@gmail.com>
2021-10-10 14:37:39 +0200

    More preps for hull creation

--- TLDAG.Core/Code/ParseCompiler.cs
+++ TLDAG.Core/Code/ParseCompiler.cs
@@ -7,34 +7,47 @@ using static TLDAG.Core.Exceptions;
 
 namespace TLDAG.Core.Code
 {
+    public class ParseProductionMap : IntMap<ParseProductionNode> { }
+    public class ParseTerminalMap : IntMap<ParseTerminalNode> { }
+
     public class ParseElement : IEquatable<ParseElement>, IComparable<ParseElement>
     {
-        public readonly int Production;
-        public readonly int Position;
-        public readonly int Terminal;
+        public class ElementKey : IEquatable<ElementKey>, IComparable<ElementKey>
+        {
+            public readonly int Production;
+            public readonly int Position;
+            public readonly int Terminal;
 
-        private int? hash = null;
+            private int? hash = null;
 
-        public ParseElement(int production, int position, int terminal)
+            public ElementKey(int production, int position, int terminal)
             { Production = production; Position = position; Terminal = terminal; }
 
-        private int ComputeHashCode() => Production << 21 + Position << 16 + Terminal;
-        public override int GetHashCode() => hash ??= ComputeHashCode();
+            internal ElementKey(ParseProductionNode production, int position, ParseTerminalNode terminal)
+                : this(production.Id, position, terminal.Id) { }
+
+            public override bool Equals(object? obj) => EqualsTo(obj as ElementKey);
+            public bool Equals(ElementKey? other) => EqualsTo(other);
+            public bool EqualsTo(ElementKey? other) => throw NotYetImplemented();
+            public override int GetHashCode() => hash ??= ComputeHashCode();
+            public int CompareTo(ElementKey? other) => throw NotYetImplemented();
+
+            private int ComputeHashCode() => Production << 21 + Position << 16 + Terminal;
+        }
+
+        public readonly ParseProductionNode Production;
+        public int Position => Key.Position;
+        public readonly ParseTerminalNode Terminal;
+        public readonly ElementKey Key;
+
+        public ParseElement(ParseProductionNode production, int position, ParseTerminalNode terminal)
+            { Production = production; Terminal = terminal; Key = new(production, position, terminal); }
+
+        public override int GetHashCode() => Key.GetHashCode();
         public override bool Equals(object? obj) => EqualsTo(obj as ParseElement);
         public bool Equals(ParseElement? other) => EqualsTo(other);
-
-        private bool EqualsTo(ParseElement? other)
-        {
-            if (other is null) return false;
-            if (Production != other.Production) return false;
-            if (Position != other.Position) return false;
-            return true;
-        }
-
-        public int CompareTo(ParseElement? other)
-        {
-            throw NotYetImplemented();
-        }
+        private bool EqualsTo(ParseElement? other) => Key.EqualsTo(other?.Key);
+        public int CompareTo(ParseElement? other) => Key.CompareTo(other?.Key);
 
         public static ParseElement Start(ParseProductionNode root)
         {
@@ -45,17 +58,23 @@ namespace TLDAG.Core.Code
             if (children[1] is not ParseTerminalNode terminal)
                 throw InvalidArgument(nameof(root), "Root's right child must be terminal.");
 
-            return new(root.Id, 0, terminal.Id);
+            return new(root, 0, terminal);
         }
     }
 
-    public class ParseElements : SmartSet<ParseElement>, IEquatable<ParseElements>
+    public class ParseElementMap : SmartMap<ParseElement.ElementKey, ParseElement> { }
+
+    public class ParseElements : SmartSet<ParseElement>, IEquatable<ParseElements>, IComparable<ParseElements>
     {
         public ParseElements(IEnumerable<ParseElement> elements) : base(elements) { }
         public ParseElements(ParseElement element) : base(element) { }
 
         public bool Equals(ParseElements? other) => throw NotYetImplemented();
+        public int CompareTo(ParseElements? other) => throw NotYetImplemented();
     }
+
+    public class ParseHulls : SmartMap<ParseElements, ParseElements> { }
+    public class ParseFirsts : SmartMap<ParseNodes, IntSet> { }
 
     public class ParseElementsCollector
     {
@@ -70,7 +89,7 @@ namespace TLDAG.Core.Code
     public class ParseInitTerminals : IParseNodeVisitor
     {
         private int nextId = ParseTerminalNode.NextId;
-        private readonly IntMap<ParseTerminalNode> terminals = new();
+        private readonly ParseTerminalMap terminals = new();
 
         public void Visit(ParseNode node)
         {
@@ -81,19 +100,17 @@ namespace TLDAG.Core.Code
             }
         }
 
-        public static IntMap<ParseTerminalNode> Init(ParseNode root)
+        public static ParseTerminalMap Init(ParseNode root)
         {
             ParseInitTerminals visitor = new();
-
             root.VisitDepthFirst(visitor);
-
             return visitor.terminals;
         }
     }
 
     public class ParseInitProductions : IParseNodeVisitor
     {
-        private readonly IntMap<ParseProductionNode> productions = new();
+        private readonly ParseProductionMap productions = new();
         private int nextId = 1;
 
         public void Visit(ParseNode node)
@@ -105,12 +122,10 @@ namespace TLDAG.Core.Code
             }
         }
 
-        public static IntMap<ParseProductionNode> Init(ParseNode root)
+        public static ParseProductionMap Init(ParseNode root)
         {
             ParseInitProductions visitor = new();
-
             root.VisitPreOrder(visitor);
-
             return visitor.productions;
         }
     }
@@ -118,32 +133,29 @@ namespace TLDAG.Core.Code
     public class ParseCompiler
     {
         private readonly ParseProductionNode root;
-        private readonly IntMap<ParseTerminalNode> terminals;
-        private readonly IntMap<ParseProductionNode> productions;
+        private readonly ParseTerminalMap terminals;
+        private readonly ParseProductionMap productions;
 
         private readonly int[] terminalIds;
 
-        private readonly SmartMap<ParseElements, ParseElements> hulls = new();
-        private readonly SmartMap<ParseNodes, IntSet> firsts = new();
+        private readonly ParseElementMap elements = new();
+        private readonly ParseHulls hulls = new();
+        private readonly ParseFirsts firsts = new();
 
-        public ParseCompiler(ParseNode root)
+        public ParseCompiler(ParseNode rawRoot)
         {
-            this.root = Extend(root);
+            root = Extend(rawRoot);
 
-            terminals = ParseInitTerminals.Init(this.root);
-            productions = ParseInitProductions.Init(this.root);
+            terminals = ParseInitTerminals.Init(root);
+            productions = ParseInitProductions.Init(root);
 
             terminalIds = terminals.Keys.ToArray();
         }
 
-        private static ParseProductionNode Extend(ParseNode root)
-        {
-            ParseNode[] children = { root, ParseTerminalNode.EndOfFile };
-
-            return new(ExtendedGrammarRootName, new(children));
-        }
-
         public static ParseCompiler Create(ParseNode root) => new(root);
+
+        private static ParseProductionNode Extend(ParseNode root)
+            => new(ExtendedGrammarRootName, new(new ParseNode[] { root, ParseTerminalNode.EndOfFile }));
 
         public ParseData Compile()
         {
@@ -152,17 +164,14 @@ namespace TLDAG.Core.Code
             throw NotYetImplemented();
         }
 
-        private IntSet First(ParseNodes nodes)
+        private IntSet First(ParseNodes nodes) => firsts[nodes] ??= ComputeFirst(nodes);
+
+        private IntSet ComputeFirst(ParseNodes nodes) { throw NotYetImplemented(); }
+
+        private ParseElements Hull(ParseElements elements) => hulls[elements] ??= ComputeHull(elements);
+
+        private ParseElements ComputeHull(ParseElements elements)
         {
-            throw NotYetImplemented();
-        }
-
-        private ParseElements Hull(ParseElements elements)
-        {
-            ParseElements? hull = hulls[elements];
-
-            if (hull is not null) return hull;
-
             ParseElementsCollector collector = new();
             bool modified;
 
