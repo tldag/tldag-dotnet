87a6e1c4ff4e3552aecc632703cd42a280bd72f9
rhjoerg <rhjoerg@gmail.com>
2021-10-15 10:45:54 +0200

    Added soma API to TestsBase

--- TLDAG.Test/TestsBase.cs
+++ TLDAG.Test/TestsBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using static TLDAG.Core.Exceptions;
@@ -9,10 +11,32 @@ namespace TLDAG.Test
 {
     public class TestsBase
     {
-        private string? solutionName;
+        private string? solutionName = null;
         protected string SolutionName => solutionName ??= GetSolutionName();
 
+        private DirectoryInfo? solutionDirectory = null;
+        protected DirectoryInfo SolutionDirectory => solutionDirectory ??= GetSolutionDirectory();
+
+        private DirectoryInfo? testOutputDirectory = null;
+        protected DirectoryInfo TestOutputDirectory => testOutputDirectory ??= CreateTestOutputDirectory();
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected DirectoryInfo GetTestDirectory(bool clear)
+        {
+            throw NotYetImplemented();
+        }
+
         private string GetSolutionName()
+        {
+            throw NotYetImplemented();
+        }
+
+        private DirectoryInfo GetSolutionDirectory()
+        {
+            throw NotYetImplemented();
+        }
+
+        private DirectoryInfo CreateTestOutputDirectory()
         {
             throw NotYetImplemented();
         }
