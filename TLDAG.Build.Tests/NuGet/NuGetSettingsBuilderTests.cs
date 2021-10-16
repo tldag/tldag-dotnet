21b05ce30ee3588c5e1491af4b92135ea693435e
rhjoerg <rhjoerg@gmail.com>
2021-10-16 11:06:50 +0200

    Simplified access to NuGet settings

--- TLDAG.Build.Tests/NuGet/NuGetSettingsBuilderTests.cs
+++ TLDAG.Build.Tests/NuGet/NuGetSettingsBuilderTests.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NuGet.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TLDAG.Build.NuGet;
 using TLDAG.Core.IO;
 using TLDAG.Test;
---
+++ TLDAG.Build/NuGet/NuGets.cs
@@ -0,0 +1,14 @@
+﻿using NuGet.Configuration;
+using System.IO;
+using TLDAG.Core;
+
+namespace TLDAG.Build.NuGet
+{
+    public static class NuGets
+    {
+        public static ISettings GetSettings(DirectoryInfo directory)
+            => global::NuGet.Configuration.Settings.LoadDefaultSettings(directory.FullName);
+
+        public static ISettings Settings => GetSettings(Env.CurrentDirectory);
+    }
+}
