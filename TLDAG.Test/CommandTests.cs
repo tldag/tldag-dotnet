58a1b7585a3e7d108413df217e846644415b5203
rhjoerg <rhjoerg@gmail.com>
2021-10-16 17:41:45 +0200

    Code cleanup

--- TLDAG.Test/CommandTests.cs
+++ TLDAG.Test/CommandTests.cs
@@ -1,10 +1,6 @@
 ﻿#if NET5_0_OR_GREATER
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TLDAG.Automation;
 
 namespace TLDAG.Test
