973a7e14dcc2b5fe950f6871bb8d3abf43d885f9
rhjoerg <rhjoerg@gmail.com>
2021-10-16 17:04:02 +0200

    Added FileInfo.ReadAllText and FileInfo.WriteAllText

--- TLDAG.Core/IO/Files.cs
+++ TLDAG.Core/IO/Files.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using TLDAG.Core.Collections;
 using static TLDAG.Core.Exceptions;
 using static TLDAG.Core.Strings;
@@ -24,6 +25,12 @@ namespace TLDAG.Core.IO
 
         public static DirectoryInfo GetDirectory(this FileInfo file)
             => file.Directory ?? throw FileNotFound(file);
+
+        public static string ReadAllText(this FileInfo file, Encoding? encoding = null)
+            { encoding ??= Encoding.UTF8; return File.ReadAllText(file.FullName, encoding); }
+
+        public static void WriteAllText(this FileInfo file, string text, Encoding? encoding = null)
+            { encoding ??= Encoding.UTF8; File.WriteAllText(file.FullName, text, encoding); }
 
         public static byte[] ReadAllBytes(this FileInfo file) => File.ReadAllBytes(file.FullName);
         public static void WriteAllBytes(this FileInfo file, byte[] bytes) { File.WriteAllBytes(file.FullName, bytes); }
