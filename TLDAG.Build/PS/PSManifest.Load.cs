779df0cc9958286cefe3ad1d01c7cef1c66fca25
rhjoerg <rhjoerg@gmail.com>
2021-10-16 17:42:48 +0200

    Added some PS manifest code

---
+++ TLDAG.Build/PS/PSManifest.Load.cs
@@ -0,0 +1,34 @@
+﻿#if NET5_0_OR_GREATER
+
+using System.IO;
+using System.Management.Automation.Language;
+using TLDAG.Core.IO;
+using static TLDAG.Core.Exceptions;
+
+namespace TLDAG.Build.PS
+{
+    public partial class PSManifest
+    {
+        public static PSManifest Load(FileInfo file) => Parse(file.ReadAllText());
+
+        public static PSManifest Parse(string text)
+        {
+            ScriptBlockAst ast = Parser.ParseInput(text, out Token[] tokens, out ParseError[] errors);
+
+            if (errors.Length > 0) throw InvalidArgument(nameof(text), errors[0].Message);
+
+            Ast? data = ast.Find(a => a is HashtableAst, false);
+
+            if (data == null) throw InvalidArgument(nameof(text), "No data");
+
+            return Parse(data.SafeGetValue());
+        }
+
+        private static PSManifest Parse(object value)
+        {
+            throw NotYetImplemented();
+        }
+    }
+}
+
+#endif
---
+++ TLDAG.Build/PS/PSManifest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TLDAG.Core.IO;
+using static TLDAG.Core.Exceptions;
+
+namespace TLDAG.Build.PS
+{
+    public partial class PSManifest
+    {
+    }
+}
