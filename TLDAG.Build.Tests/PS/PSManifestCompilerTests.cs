3e1713b0d98c3ecb37e8522dbb732cab52bcc2a1
rhjoerg <rhjoerg@gmail.com>
2021-10-17 17:03:43 +0200

    Work on PS manifest compiler

---
+++ TLDAG.Build.Tests/PS/PSManifestCompilerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLDAG.Build.Tests.PS
+{
+    [TestClass]
+    public class PSManifestCompilerTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
