a42178aeab7bade66ac77ec3ceca3388142f91eb
rhjoerg <rhjoerg@gmail.com>
2021-10-18 14:07:16 +0200

    Cleanup

--- eng/Build-TldagDotnet.ps1
+++ eng/Build-TldagDotnet.ps1
@@ -2,6 +2,8 @@
 Get-Process -Name dotnet | Stop-Process
 
 dotnet clean tldag-dotnet.sln -c Release -v m -noLogo
+dotnet clean tldag-dotnet.sln -c Debug -v m -noLogo
+
 dotnet build tldag-dotnet.sln -c Release -noLogo
 dotnet test tldag-dotnet.sln -c Release -r:False -noLogo
 dotnet build tldag-dotnet-samples.sln -c Release -noLogo
