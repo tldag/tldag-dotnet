3de7e75d6af190a8005f293b40d6aefc910c19f6
rhjoerg <rhjoerg@gmail.com>
2021-10-19 17:42:20 +0200

    Added DotNetInfo

---
+++ TLDAG.Build.Tests/DotNet/DotNetInfoTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Versioning;
+using TLDAG.Build.DotNet;
+using TLDAG.Core;
+
+namespace TLDAG.Build.Tests.DotNet
+{
+    [TestClass]
+    public class DotNetInfoTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            DotNetInfo info = DotNetInfo.Get(Env.WorkingDirectory);
+            SemanticVersion expectedVersion = new(5, 0, 402);
+            SemanticVersion actualVersion = info.Version;
+
+            Assert.AreEqual(expectedVersion, actualVersion);
+            Assert.IsTrue(info.BaseDirectory.Exists);
+        }
+    }
+}
---
+++ TLDAG.Build/DotNet/DotNetInfo.cs
@@ -0,0 +1,68 @@
+﻿using NuGet.Versioning;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TLDAG.Core;
+using TLDAG.Core.Reflection;
+using static TLDAG.Core.Strings;
+using static TLDAG.Build.Resources.DotNetResources;
+
+namespace TLDAG.Build.DotNet
+{
+    public class DotNetInfo
+    {
+        public Executable Executable { get; }
+        public SemanticVersion Version { get; }
+        public DirectoryInfo BaseDirectory { get; }
+
+        private DotNetInfo(Executable executable, SemanticVersion version, DirectoryInfo baseDirectory)
+        {
+            Executable = executable;
+            Version = version;
+            BaseDirectory = baseDirectory;
+        }
+
+        public static DotNetInfo Get(DirectoryInfo directory)
+        {
+            ExecutionResult result = ExecutionBuilder.Create("dotnet")
+                .UseShellExecute(false)
+                .WorkingDirectory(directory)
+                .AddArgument("--info")
+                .Build().Execute(true);
+
+            List<string> trimmed = result.Outputs.Select(line => line.Trim()).ToList();
+            SemanticVersion version = ParseVersion(trimmed);
+            DirectoryInfo baseDirectory = ParseBaseDirectory(trimmed);
+
+            return new(result.Executable, version, baseDirectory);
+        }
+
+        public const string VersionLinePrefix = "Version:";
+
+        private static SemanticVersion ParseVersion(IEnumerable<string> lines)
+        {
+            string? versionLine = lines
+                .Where(line => line.StartsWith(VersionLinePrefix, OrdinalComparison))
+                .FirstOrDefault();
+
+            string parseable = Contract.State.NotNull<string>(versionLine, DotNetInfoNoVersion)
+                .Substring(VersionLinePrefix.Length).Trim();
+
+            return SemanticVersion.Parse(parseable);
+        }
+
+        public const string BasePathPrefix = "Base Path:";
+
+        private static DirectoryInfo ParseBaseDirectory(IEnumerable<string> lines)
+        {
+            string? basePathLine = lines
+                .Where(line => line.StartsWith(BasePathPrefix, OrdinalComparison))
+                .FirstOrDefault();
+
+            string parseable = Contract.State.NotNull<string>(basePathLine, DotNetInfoNoBasePath)
+                .Substring(BasePathPrefix.Length).Trim();
+
+            return new(parseable);
+        }
+    }
+}
