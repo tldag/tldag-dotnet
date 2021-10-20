55a012cb428fd844f465b65a3500ab1beec40af3
rhjoerg <rhjoerg@gmail.com>
2021-10-20 15:04:41 +0200

    Small pipe experiments

---
+++ TLDAG.Build.Tests/AnonymousPipesTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace TLDAG.Build.Tests
+{
+    [TestClass]
+    public class AnonymousPipesTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            using AnonymousPipeServerStream server = new(PipeDirection.In);
+            string handle = server.GetClientHandleAsString();
+            using AnonymousPipeClientStream client = new(PipeDirection.Out, handle);
+
+            byte expected = 5;
+
+            client.WriteByte(expected);
+
+            byte actual = (byte)server.ReadByte();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
