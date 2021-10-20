d9b056678f2b2be6f33f7af46b63455b430fa3b0
rhjoerg <rhjoerg@gmail.com>
2021-10-20 11:18:48 +0200

    Kill dotnet instances after build

--- eng/Build-TldagDotnet.ps1
+++ eng/Build-TldagDotnet.ps1
@@ -7,3 +7,5 @@ dotnet clean tldag-dotnet.sln -c Debug -v m -noLogo
 dotnet build tldag-dotnet.sln -c Release -noLogo
 dotnet test tldag-dotnet.sln -c Release -r:False -noLogo
 dotnet build tldag-dotnet-samples.sln -c Release -noLogo
+
+Get-Process -Name dotnet -ErrorAction Ignore | Stop-Process
