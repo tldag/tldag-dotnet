bbeb0146058e1969de89869aeab840184a705bec
rhjoerg <rhjoerg@gmail.com>
2021-11-01 08:00:51 +0100

    Added some love to String.ToLines

--- TLDAG.Core/Strings.cs
+++ TLDAG.Core/Strings.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using TLDAG.Core.Internal;
+using System.Text;
 
 namespace TLDAG.Core
 {
@@ -24,11 +25,24 @@ namespace TLDAG.Core
         public static bool IsDigit(this char c) => c >= '0' && c <= '9';
         public static bool IsDigits(this string chars) => chars.All(c => c.IsDigit());
 
-        public static IEnumerable<string> ToLines(this string text) => new StringToLines(text);
-
         public static string Join(this IEnumerable<string> strings, string separator)
             => string.Join(separator, strings);
 
         public static string Format(this string format, params object[] args) => string.Format(format, args);
+
+        public static StreamReader GetReader(this Stream stream, Encoding? encoding = null)
+            => encoding is null ? new(stream) : new(stream, encoding);
+
+        public static StringReader GetReader(this string text)
+            => new(text);
+
+        public static IEnumerable<string> GetLines(this TextReader reader)
+            { while (reader.ReadLine() is string line) yield return line; }
+
+        public static IEnumerable<string> GetLines(this Stream stream, Encoding? encoding = null)
+            => stream.GetReader(encoding).GetLines();
+
+        public static IEnumerable<string> GetLines(this string text)
+            => text.GetReader().GetLines();
     }
 }
