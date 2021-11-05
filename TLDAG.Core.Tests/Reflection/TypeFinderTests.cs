730e5da4702f12028e1c9c2a62c7579c52dea4bb
rhjoerg <rhjoerg@gmail.com>
2021-11-05 10:46:32 +0100

    Added a rudimentary type finder

---
+++ TLDAG.Core.Tests/Reflection/TypeFinderTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using TLDAG.Core.Reflection;
+
+namespace TLDAG.Core.Tests.Reflection
+{
+    [TestClass]
+    public class TypeFinderTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Type cSharp = typeof(CSharpSyntaxNode);
+            Assembly assembly = cSharp.Assembly;
+            List<TypeInfo> types = new();
+            IEnumerable<TypeInfo> candidates;
+
+            candidates = TypeFinder.Create(assembly).BaseType(cSharp).Find();
+
+            while (candidates.Any())
+            {
+                types.AddRange(candidates.Where(c => !c.IsAbstract));
+
+                candidates = candidates
+                    .Where(c => c.IsAbstract)
+                    .SelectMany(c => TypeFinder.Create(assembly).BaseType(c).Find());
+            }
+
+            types.ForEach(t => Debug.WriteLine(t.FullName));
+        }
+    }
+}
---
+++ TLDAG.Core/Reflection/Types.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TLDAG.Core.Reflection
+{
+    public class TypeFinder
+    {
+        private readonly List<Assembly> assemblies = new();
+        private Type? baseType = null;
+
+        public TypeFinder(params Assembly[] assemblies) : this(assemblies.AsEnumerable()) { }
+        public TypeFinder(IEnumerable<Assembly> assemblies) { this.assemblies.AddRange(assemblies); }
+
+        public static TypeFinder Create(params Assembly[] assemblies) => new(assemblies);
+        public static TypeFinder Create(IEnumerable<Assembly> assemblies) => new(assemblies);
+
+        public TypeFinder BaseType(Type? type) { baseType = type; return this; }
+
+        public IEnumerable<TypeInfo> Find()
+        {
+            return assemblies
+                .SelectMany(a => a.DefinedTypes)
+                .Where(HasBaseType);
+        }
+
+        private bool HasBaseType(TypeInfo type)
+            => baseType is null || AreTypesEqual(baseType, type.BaseType);
+
+        private static bool AreTypesEqual(Type? a, Type? b)
+        {
+            if (a is null) return b is null;
+            if (b is null) return false;
+
+            string? aName = a.FullName;
+            string? bName = b.FullName;
+
+            if (aName is null) return bName is null;
+            if (bName is null) return false;
+
+            return aName.Equals(bName);
+        }
+    }
+}
