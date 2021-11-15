4dfc8495a011f45c47b6d8e92dd2dca398df9b2d
rhjoerg <rhjoerg@gmail.com>
2021-11-15 18:59:17 +0100

    More cleanup

--- TLDAG.DotNetLogger/Adapter/CustomEventAdapter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Build.Framework;
-
-namespace TLDAG.DotNetLogger.Adapter
-{
-    public class CustomEventAdapter : BuildAdapter<CustomBuildEventArgs>
-    {
-        public CustomEventAdapter(CustomBuildEventArgs args) : base(args) { }
-    }
-}
